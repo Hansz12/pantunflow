236b7df10eff9b3312461dacf5497308c83e077d
Izatul28 <izzatulfitrahhroslan@gmail.com>
2026-07-24 16:49:47 +0800

    izatul

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,24 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart'; // 1. Pastikan import ini ada
+import 'screens/login_page.dart';
 
-import 'app.dart';
+void main() async {
+  // 2. Wajib ada dua baris ini di paling atas main()
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(); 
 
-void main() => runApp(const PantunFlowApp());
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'PantunFlow',
+      home: LoginPage(),
+    );
+  }
+}
